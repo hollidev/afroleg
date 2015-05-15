5ce76ef33eac44b218912e7ce2b5a77d503fd408
unknown <r1101256@TA-VIULU32.LUC.LOCAL>
2015-05-15 23:35:33 +0300

    menuvalikko toimii

---
+++ Assets/Code/Buttons.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartButton(){
+		Application.LoadLevel ("eka"); //start game
+	}
+
+	public void ExitButton(){
+		Application.Quit (); //exit game
+	}
+
+}
