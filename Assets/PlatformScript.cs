26dafd5ad22b7e98f0202c3155a10f99b9e79014
unknown <a1300934@TA-VIULU33.LUC.LOCAL>
2015-05-15 22:40:43 +0300

    yhdenmukaistus

---
+++ Assets/PlatformScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformScript : MonoBehaviour {
+	Random rn;
+	GameObject littleAfro;
+	// Use this for initialization
+	void Start () {
+		rn = new Random();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		littleAfro = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+		littleAfro.transform.position = Vector3 ();
+	}
+}
