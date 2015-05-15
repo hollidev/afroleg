a09d17b335b3e86c031e89e09daa75185b29aa6c
unknown <a1300934@TA-VIULU33.LUC.LOCAL>
2015-05-16 00:07:55 +0300

    jo

--- Assets/Code/PlatformScript.cs
+++ Assets/Code/PlatformScript.cs
@@ -2,19 +2,47 @@
 using System.Collections;
 
 public class PlatformScript : MonoBehaviour {
-	Random rn;
+
 	GameObject littleAfro;
+	int updateCounter = 0;
+	float scale;
+	float collisionTime = 0f;
+	float fallSpeed = 0f;
+	int afroCounter;
+	float pos;
+
+	
 	// Use this for initialization
 	void Start () {
-		rn = new Random();
-
+		scale = transform.localScale.x;
+		littleAfro = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		Rigidbody rb = littleAfro.AddComponent<Rigidbody>();
+		rb.useGravity = false;
+		rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		littleAfro = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		updateCounter ++;
 
-		//littleAfro.transform.position = Vector3 ();
+		if (updateCounter % 100 == 0) {
+			Instantiate(littleAfro);
+			afroCounter++;
+			pos = Random.Range (scale/2*(-1), scale/2);
+			littleAfro.transform.position = new Vector3 (pos,30f);
+			fallSpeed = Random.Range (1f, 10f);
+
+		}
+		//GameObject lilAfro = GameObject.FindGameObjectWithTag ("afro1");
+		//lilAfro.transform.position += Vector3.down * 5.0f * Time.deltaTime;
 	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name != "pallo" && col.gameObject != "laattia") {
+			Destroy(gameObject,.5f);
+		}
+	}
+
+
 }
