0a1afbe7a777da5d27cf57b354582e68f923e9ee
unknown <r1101256@TA-VIULU32.LUC.LOCAL>
2015-05-17 13:21:16 +0300

    pisteitä ja iconit androidille

---
+++ Assets/Code/H_Score.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class H_Score : MonoBehaviour {
+
+	public Text[] pisteet;
+
+
+	// Use this for initialization
+	void Start () {
+
+		string key = "key";
+		//int luku;
+		string lukuS;
+
+		for(int i=9; i>-1; i--)
+		{
+			
+			key = "key" + i;
+
+			//luku = PlayerPrefs.GetInt(key);
+
+			lukuS = PlayerPrefs.GetInt(key).ToString();
+
+			pisteet[i].text = lukuS;
+
+			Debug.Log("pisteideksi: " + pisteet[i] + "tallennettu muisti: " + lukuS);
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Code/Score.cs
+++ Assets/Code/Score.cs
@@ -6,20 +6,78 @@ public class Score : MonoBehaviour {
 
 	public Canvas scoreCanvas;
 	int score;
+	int highScore;
 	public Text scoreText;
+	//public Text[] pisteet;
+	
 
 	void Start(){
 		scoreCanvas.enabled = false;
 
 		score = PlayerPrefs.GetInt ("endScore");
 		Debug.Log ("Endscore: " + score);
+
+		scoreText.text = "" + score;
+
+
 		//TODO
 		//tarkistaa onko pistemäärä riittävän suuri tilastolle pääsyyn, jos on
 		//pitää canvas enabloida
 
-		scoreText.text = "" + score;
+		/*int pistemaara;
+		Debug.Log("int pisteille luotu, ei vielä for luuppiin"+pisteet.Length);
+		for(int i=0; i<pisteet.Length; i++)
+		{
+			pistemaara = int.Parse(pisteet[i].text);
+			Debug.Log("" + pistemaara);
+
+		}*/
+
+		//int asetusmuisti = 0;
+		string key = "key";
+		int keyscore;
+
+		for(int i=9; i>-1; i--)
+		{
+			/*tämä on vain sitä varten, että sain jotain lukuja noihin muisteihin
+			 * 
+			key = "key" + i;
+			Debug.Log(key);
+			PlayerPrefs.SetInt (key, asetusmuisti);
+			asetusmuisti = asetusmuisti + 5;*/
+
+			key = "key" + i;
+			keyscore = PlayerPrefs.GetInt(key);
+			int pudotusmuisti;
+
+
+			//tämän sisään jos pistetulos pelistä on suurempi kuin arvo tilastossa
+			if(keyscore < score)
+			{
+				//siirretään korvattava arvo talteen
+				pudotusmuisti = keyscore;
+
+				
+				//alhaaltalukien siirretään pienemmät luvut aina alempaan muistiin
+				string toinen;
+				for(int j = 0; j < i; j++){
+					key="key"+j;
+					toinen="key"+j++;
+					PlayerPrefs.SetInt(key, PlayerPrefs.GetInt(toinen));
+					}
+
+				//tallennetaan muistipaikkaan score
+				key = "key" + i;
+				PlayerPrefs.SetInt(key, score);
+			}
+
+		}
+
+
 	}
-	
+
+
+
 	void Update()
 	{
 
